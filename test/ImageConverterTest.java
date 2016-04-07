04ade71fedeccd6f6a09feb7c84c2674d77f6c45
Brian[laptop] <bwijayan@ryerson.ca>
2016-04-07 17:49:09 -0400

    Removed HelloControllerTest. Updated ImageConverter Tests.

--- test/ImageConverterTest.java
+++ test/ImageConverterTest.java
@@ -19,44 +19,119 @@ import app.springapp.ImageConverter;
 
 /**
  *
- * @author swagbito
+ * @author CodeFletcher
  */
 public class ImageConverterTest {
     
     public ImageConverterTest() {
     }
     
+    ImageConverter img_conv=new ImageConverter();
     
     @Test
     public void testLoadImage(){
        
-    ImageConverter img_conv=new ImageConverter();
-  
-    //img_conv.setFilePath("/home/swagbito/Documents/imageDIR/lena1.png");
-    img_conv.loadImage("/home/swagbito/Documents/imageDIR/lena1.png");
-    img_conv.setMessage("HelloWorldDDD");
-    img_conv.convertImage();
-    img_conv.getFileName();
-   
-   //System.out.println(img_conv.deCryptMessage(true));
     
-   System.out.println(img_conv.getBlockValue(400, 0, 45, true));
-   img_conv.adjustBlockValue(30, 45, 400, 0);
-   System.out.println(img_conv.getBlockValue(400, 0, 45, true));
-  
-    //BufferedImage x=img_conv.getOriginalImage();
-    //System.out.println(x.getWidth());
+    img_conv.loadImage("/home/swagbito/NetBeansProjects/springapp/test/finaltest.png");
     
-     JLabel picLabel = new JLabel(new ImageIcon(img_conv.getOriginalImage()));
-     JPanel panel =new JPanel();
-     JFrame frame=new JFrame();
-     picLabel.setBounds(0,0,600,500);
-     panel.add(picLabel);
-     
-     
-     frame.add(panel);
-     frame.setBounds(0, 0, 600, 600);
-     frame.setVisible((true));
+    BufferedImage im=img_conv.getOriginalImage();
+    boolean load;
+    if(im!=null){
+         load=true;
+    }else{
+        load=false;
+    }
+    
+    assertEquals(load,true);
+       
+    }
+    
+    @Test
+    public void testGetFileNameFromPath(){
+        
+        String s="/home/swagbito/NetBeansProjects/springapp/test/finaltest.png";
+        
+        String name=img_conv.getFileNameFromPath(s);
+        
+        assertEquals(name,"finaltest.png");
+        
+    }
+    
+    @Test
+    public void testGetMaxEncryptionSize(){
+        
+        testLoadImage();
+        
+              
+        assertEquals(img_conv.getMaxEncryptionSize(),1999);
+    }
+    
+    @Test
+    public void testGetXY(){
+        
+        testLoadImage();
+        int x=0,y=0,n=45;
+        
+        int x2=img_conv.getX(x, y, n);
+        int y2=img_conv.getY(x, y, n);
+        assertEquals(x2,44);
+        assertEquals(y2,0);
+        
+        //test boundary image width is 300;
+        x=295; y=104; n=45;
+        x2=img_conv.getX(x, y, n);
+        y2=img_conv.getY(x, y, n);
+        
+        assertEquals(x2,39);
+        assertEquals(y2,105);
+        
+    }
+    
+    @Test
+    public void testGetBlockValue(){
+        
+        testLoadImage();
+        int x=0,y=0,n=45;
+        
+        int block=img_conv.getBlockValue(x, y, n, false);
+              
+        assertEquals(block,72);
+                
+    }
+    
+     @Test 
+    public void testAdjustBlockValue(){
+        
+        testLoadImage();
+        int x=0,y=0,n=45;
+              
+        img_conv.copyOriginalImage();
+        img_conv.adjustBlockValue(100, n, x, y);
+        
+        int block=img_conv.getBlockValue(x, y, n, true);
+        assertEquals(block,100);
+        
+    }
+    
+    @Test
+    public void testDeCrypt(){
+        
+        testLoadImage();
+        img_conv.deCryptMessage(false);
+        
+        assertEquals(img_conv.getMessage(),"Hello I'm a plant. Thats pretty much it. I'm also green... I like the sunshine too. Well thats enough about me.");
+        
+    }
+    
+    @Test
+    public void testEncrypt(){
+        
+      img_conv.loadImage("/home/swagbito/NetBeansProjects/springapp/test/finaltest_encrypt.png");
+      img_conv.setMessage("Hello are you working?");
+      img_conv.convertImage();
+      
+      assertEquals(img_conv.deCryptMessage(true),"Hello are you working?");
+        
     }
     
 }
